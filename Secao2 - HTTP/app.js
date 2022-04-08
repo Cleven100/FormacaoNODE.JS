d6d299a580366c4c60f816714150f6d64e950928
Cleven Nesello <83832324+Cleven100@users.noreply.github.com>
2022-04-08 10:18:45 -0300

    08/04

--- Secao2 - HTTP/app.js
+++ Secao2 - HTTP/app.js
@@ -1,7 +1,7 @@
 let http = require("http");
 
 http.createServer(function(requisicao,resposta){
-    resposta.end("Bem vindo ao meu site!");
+    resposta.end("<h1>Bem vindo ao meu site!<h1>");
 }).listen(8181);
 
 console.log("Meu servidor está rodando");
