9d89e4001ab4b07ab582e821988d4bda498da5ec
Cleven Nesello <83832324+Cleven100@users.noreply.github.com>
2022-04-08 08:23:49 -0300

    08/04

---
+++ Secao1 - Introducao ao Node.js/app.js
@@ -0,0 +1,39 @@
+console.log("hello world!");
+const site = 'www.google.com'
+
+var mostrarSite = false;
+
+
+
+if(mostrarSite){
+    console.log(site);
+}
+
+
+
+function soma(a,b) {
+    return a + b;
+}
+
+
+function multiplicacao(a,b) {
+    return a * b;
+}
+
+function subtracao(a,b) {
+    return a - b ;
+}
+
+function divisao(a , b) {
+    return a/b;
+}
+
+console.log(soma(10,20));
+let resultado = multiplicacao(10,10);
+console.log(resultado)
+let resultadoMultiplicacao = soma(resultado,20);
+console.log(resultadoMultiplicacao);
+console.log(divisao(resultadoMultiplicacao,2));
+
+
+
