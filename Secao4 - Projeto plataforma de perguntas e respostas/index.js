a2544a3158fa9040478312708a7f678fea007273
Cleven Nesello <83832324+Cleven100@users.noreply.github.com>
2022-04-26 11:25:55 -0300

    26/04

--- Secao4 - Projeto plataforma de perguntas e respostas/index.js
+++ Secao4 - Projeto plataforma de perguntas e respostas/index.js
@@ -8,12 +8,22 @@ app.get("/:nome/:lang", (require, response) => {
     var nome = require.params.nome;
     var lang = require.params.lang;
     var exibirMensagem = false;
+
+    var products =[
+        {nome: "Doritos", preco: 3.14},
+        {nome: "Fandangos", preco: 2.00},
+        {nome: "coca-cola", preco: 3.00},
+        {nome: "Leite Elege", preco: 4.99},
+        {nome: "Manteiga", preco: 5.99}
+    ]
+
     response.render("index", {
         nome: nome,
         lang: lang,
         empresa: "Guia do programador",
         inscritos: 8000,
-        msg: exibirMensagem
+        msg: exibirMensagem,
+        products: products
     });
 });
 
