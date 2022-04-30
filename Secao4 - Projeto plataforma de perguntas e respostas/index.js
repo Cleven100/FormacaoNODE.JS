2e13ffcf7408f3e9afa66fca131ace5eb4c81300
Cleven Nesello <83832324+Cleven100@users.noreply.github.com>
2022-04-30 15:57:08 -0300

    30/04

--- Secao4 - Projeto plataforma de perguntas e respostas/index.js
+++ Secao4 - Projeto plataforma de perguntas e respostas/index.js
@@ -1,15 +1,27 @@
 const express = require("express");
 const app = express();
+const bodyParser = require("body-parser");
 
 app.set("view engine", "ejs");
 app.use(express.static("public"));
 
+//bodyParse
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+//rotas
 app.get("/", (require, response) => {
   response.render("index");
 });
 
-app.get("/perguntar", (require, response) => {
+app.get("/question", (require, response) => {
     response.render("perguntar");
+})
+
+app.post("/saveQuestion",(require, response) => {
+  let title = require.body.title;
+  let description = require.body.description;
+  
+  response.send(`Formulário recebido com sucess ${title} ${description}`);
 })
 
 app.listen(8080, () => {
