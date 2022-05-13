adc611fb229ea515f3beaff6b69600d9606fb200
Cleven Nesello <83832324+Cleven100@users.noreply.github.com>
2022-05-13 15:23:16 -0300

    13/05

--- Secao4 - Projeto plataforma de perguntas e respostas/index.js
+++ Secao4 - Projeto plataforma de perguntas e respostas/index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const app = express();
 const bodyParser = require("body-parser");
 const connection = require('./database/database');
+const perguntaModel = require('./database/Pergunta')
 
 //DataBase
 
