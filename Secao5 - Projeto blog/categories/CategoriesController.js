6df13411970cc9674cef94274784c0c1d8f9f965
Cleven Nesello <83832324+Cleven100@users.noreply.github.com>
2022-08-02 10:32:42 -0300

    create new/admin

--- Secao5 - Projeto blog/categories/CategoriesController.js
+++ Secao5 - Projeto blog/categories/CategoriesController.js
@@ -1,13 +1,8 @@
 const express = require("express");
 const router = express.Router();
 
-router.get("/categories", (require, response) => {
-    response.send("ROTA DE CATEGORIAS")
-});
-
 router.get("/admin/categories/new", (require, response) => {
-    response.send("ROTA DE CATEGORIAS ADMIN")
+    response.render("admin/categories/new");
 });
-
 
 module.exports = router;
